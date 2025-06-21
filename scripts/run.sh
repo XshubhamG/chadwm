114f415ef21a7acf39a3cb3b382982ab96db0486
Shubham Giri <shubhammgiri3@gmail.com>
2025-06-21 23:17:40 +0530

    udiskie support

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,8 @@
 
 xrdb merge ~/.Xresources
 # picom -b &
+/usr/lib/polkit-kde-authentication-agent-1 &
+udiskie -2 &
 feh --bg-fill ~/Pictures/wallpaper/scenery_tower.jpg &
 dunst &
 xset r rate 200 50 &
